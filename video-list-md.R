3f0368701d1ea81a7826c77a987e5b057f1429d5
Michael Ching <mikeching@yahoo.com>
2013-06-16 20:27:04 -1000

    initial commit

---
+++ video-list-md.R
@@ -0,0 +1,15 @@
+# Script to generate the text and links for a video download markup page
+
+# Number of Units and number of Modules per unit
+module <- c(6, 6, 5, 7, 8, 8, 6, 7, 6)
+
+for(i in 1:9) {
+  print(paste0("##Unit ", i))
+  for(j in 1:module[i]) {
+    text <- paste0("[Unit ", i, ", Module ", j, "]")
+    address <- paste0("(https://s3-us-west-1.amazonaws.com/prod-edx/MedStats/Video240p/", "Unit+", i, "+Module+", j ,".mp4)")
+    print(paste0(text, address))
+  }
+}
+
+links
