bbe5a1b52c44d20a5cc719134659b5658848d22b
Michael Ching <mikeching@yahoo.com>
2013-06-22 07:05:46 -1000

    add teaser, remove links

--- video-list-md.R
+++ video-list-md.R
@@ -5,11 +5,12 @@ module <- c(6, 6, 5, 7, 8, 8, 6, 7, 6)
 
 for(i in 1:9) {
   print(paste0("##Unit ", i))
-  for(j in 1:module[i]) {
+  for(j in 0:module[i]) {
+    if(j == 0) {
+      text <- paste0("[Unit ", i, ", Overview/Weekly Teaser]")
+    }
     text <- paste0("[Unit ", i, ", Module ", j, "]")
-    address <- paste0("(https://s3-us-west-1.amazonaws.com/prod-edx/MedStats/Video240p/", "Unit+", i, "+Module+", j ,".mp4)")
+    address <- paste0("(https://s3-us-west-1.amazonaws.com/prod-edx/MedStats/Video240p/", "Unit+", i, "+Module+", j ,".mp4)  ")
     print(paste0(text, address))
   }
 }
-
-links
