dad34239abe45e51beba79de6d89926377123b26
Michael Ching <mikeching@yahoo.com>
2013-06-22 07:07:48 -1000

    correct teaser links

--- video-list-md.R
+++ video-list-md.R
@@ -4,13 +4,16 @@
 module <- c(6, 6, 5, 7, 8, 8, 6, 7, 6)
 
 for(i in 1:9) {
-  print(paste0("##Unit ", i))
+  print(paste0("##Unit ", i, "  "))
   for(j in 0:module[i]) {
     if(j == 0) {
       text <- paste0("[Unit ", i, ", Overview/Weekly Teaser]")
-    }
+      address <- paste0("(https://s3-us-west-1.amazonaws.com/prod-edx/MedStats/Video240p/", "Unit+", i, "+Module+", j ,".mp4)  ")
+      print(paste0(text, address))
+    } else {
     text <- paste0("[Unit ", i, ", Module ", j, "]")
     address <- paste0("(https://s3-us-west-1.amazonaws.com/prod-edx/MedStats/Video240p/", "Unit+", i, "+Module+", j ,".mp4)  ")
     print(paste0(text, address))
+    }
   }
 }
