82565ed581646b7eaa83269c4d6b2a5616860e57
Michael Ching <mikeching@yahoo.com>
2013-06-25 21:49:43 -1000

    do not use this file, use Rmd file instead

---
+++ week3/unit-3-mod-3-quiz.R
@@ -0,0 +1,11 @@
+Unit 3 Mod 3 Quiz
+====
+
+### Suppose that 15% of the population was exposed to a dangerous chemical Z. If people who were not exposed to Z have a lifetime risk of pancreatic cancer of .0001 and people who were exposed to Z have a lifetime risk of .0004, what percent of the population will develop pancreatic cancer (e.g., what is the marginal probability of pancreatic cancer)?  
+
+Use the law of total probability.
+$$P(A|B)*P(B) + P(A|~B)P(~B)$$
+
+```{r}
+
+```
