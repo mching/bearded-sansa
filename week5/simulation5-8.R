ca9690320e863f1663cfc51289a0dce047df6c5f
Michael Ching <mikeching@yahoo.com>
2013-06-29 22:36:19 -1000

    initial commit

---
+++ week5/simulation5-8.R
@@ -0,0 +1,12 @@
+x <- rep(NA, 15000)
+set.seed(96859)
+for(i in 1:15000) {
+  x[i] <- diff(table(rbinom(125, 1, prob = 0.5)))
+}
+mean(x)
+sd(x)
+sum(abs(x)>=23)/15000
+
+
+pbinom(51, 125, 0.5) # one sided
+2 * pbinom(51, 125, 0.5) # two sided
