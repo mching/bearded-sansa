980b0b36c41328c691cfd5f2c89d2af8ad6efb99
Michael Ching <mikeching@yahoo.com>
2013-07-01 21:15:15 -1000

    initial commit

---
+++ week4/simulation-4-2-OR.R
@@ -0,0 +1,23 @@
+# Simulate OR
+# Lecture 4-2 HRP 258
+
+# Generate sample data
+# n = 1000 cases and n = 1000 controls
+# 20% cases are smokers and 20% controls are smokers
+
+dat <- data.frame(disease = rep(0:1, c(1000,1000)), smoker = rep(c(1,0,0,0,0), 400))
+dat$disease <- factor(dat$disease, levels = 1:0, labels = c("case", "control"))
+dat$smoker <- factor(dat$smoker, levels = 1:0, labels = c("smoker", "non-smoker"))
+table(dat)
+
+# Make variable to store results
+OR.data <- rep(NA, 1000)
+
+for(i in 1:1000) {
+  # Sample 50 cases and 50 controls
+  cases.index <- sample(1000, 50, replace = F)
+  controls.index <- sample(1000, 50, replace = F) + 1000L
+  table(dat[c(cases.index, controls.index), ])
+  # Calculate OR
+  # Store in variable  
+}
