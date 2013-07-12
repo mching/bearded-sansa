b5e50ef907855610283248d2b06cf1fdf620fd25
Michael Ching <mikeching@yahoo.com>
2013-07-12 10:00:37 -1000

    more elegant method

--- week5/simulation5-8.R
+++ week5/simulation5-8.R
@@ -1,13 +1,9 @@
-x <- rep(NA, 15000)
 set.seed(96859)
-for(i in 1:15000) {
-  x[i] <- 2*(62.5 - sum(rbinom(125, 1, prob = 0.5)))
-}
+x <- 125 - 2 * rbinom(15000, 125, 0.5)
 mean(x)
 sd(x)
 hist(x)
 sum(abs(x)>=23)/15000
-
 
 pbinom(51, 125, 0.5) # one sided
 2 * pbinom(51, 125, 0.5) # two sided
